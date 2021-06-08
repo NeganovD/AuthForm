d771ddfccb3d4c4da33e112db5dd78994830a415
NeganovD <85576644+NeganovD@users.noreply.github.com>
2021-06-09 01:03:02 +0400

    Update checkbrowser.js

--- checkbrowser.js
+++ checkbrowser.js
@@ -1,9 +1,8 @@
-if (navigator.userAgent.search(/Safari/) > 0) { a = 'Safari' };
-if (navigator.userAgent.search(/Firefox/) > 0) { a = 'MozillaFirefox' };
-if (navigator.userAgent.search(/MSIE/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) { a = 'Internet Explorer' };
-if (navigator.userAgent.search(/Chrome/) > 0) { a = 'Google Chrome' };
-if (navigator.userAgent.search(/YaBrowser/) > 0) { a = 'Яндекс браузер' };
-if (navigator.userAgent.search(/OPR/) > 0) { a = 'Opera' };
-if (navigator.userAgent.search(/Edge/) > 0) { a = 'Microsoft Edge' };
-if (a == 'Internet Explorer')
-    document.location.href = "invalidbrowser.html";
+navigator.userAgent.search();
+console.log(navigator.userAgent);//выводим имя браузера и ищем то, что может указывать на IE
+if (navigator.userAgent.search(/MSIE/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) 
+{ a = 'Internet Explorer' };// если мы находим в строке признаки IE то значение а даем название IE
+if (a == 'Internet Explorer') {
+    alert('недопустимый браузер')
+    document.execCommand("Stop")//останавливает загрузку после вызова скрипта, поэтому этот скрипт лежит вверху body
+}

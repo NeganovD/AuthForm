05484b61ff14ef5c05511de78f55099047077e11
NeganovD <85576644+NeganovD@users.noreply.github.com>
2021-06-09 12:49:47 +0400

    Update checkbrowser.js

--- checkbrowser.js
+++ checkbrowser.js
@@ -1,6 +1,6 @@
 navigator.userAgent.search();
 console.log(navigator.userAgent);//выводим имя браузера и ищем то, что может указывать на IE
-if (navigator.userAgent.search(/MSIE/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) 
+if (navigator.userAgent.search(/.NET4/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) 
 { a = 'Internet Explorer' };// если мы находим в строке признаки IE то значение а даем название IE
 if (a == 'Internet Explorer') {
     alert('недопустимый браузер')

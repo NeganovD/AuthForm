1824d1fd6c8e31553f2a4318d99fb2ff56f09c82
NeganovD <85576644+NeganovD@users.noreply.github.com>
2021-06-10 11:11:53 +0400

    Update datetime.js

--- datetime.js
+++ datetime.js
@@ -12,11 +12,13 @@ nchas = NOL(datavremya.getHours());//применяем функцию для п
 nminuta = NOL(datavremya.getMinutes());
 nsekunda = NOL(datavremya.getSeconds());
 ngod = datavremya.getFullYear();
-nmesyac = NOL(datavremya.getMonth()+1); //(чтобы вывелось правильный номер месяца, т.к система считает от 0, где 0 - январь, 1 - февраль и т.д)
+//nmesyac = NOL(datavremya.getMonth()+1); //(чтобы вывелось правильный номер месяца, т.к система считает от 0, где 0 - январь, 1 - февраль и т.д)
 nden = datavremya.getDate();
 dennedeli = new Array('Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'); //создаем массив и задаем названия каждому элементу где 0 - это ВС, 1 - ПН и т.д
 vdennedeli = dennedeli[datavremya.getDay()];
-var ZZZ = nchas + ":" + nminuta + ":" + nsekunda + " Сегодня: " + vdennedeli + " "+ nden + "." + nmesyac + "." + ngod;
+imyamesyaca = new Array('Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'); //создаем массив и задаем названия каждому элементу где 0 - это ВС, 1 - ПН и т.д
+vimyamesyaca = imyamesyaca[datavremya.getMonth()];
+var ZZZ = nchas + ":" + nminuta + ":" + nsekunda + " Сегодня: " + vdennedeli + ", "+ nden + " " + vimyamesyaca + " " + ngod + " г.";
 console.log(ZZZ);
 document.getElementsByTagName('time')[0].textContent = ZZZ;
 },1000);
